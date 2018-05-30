a40122577f6625deae7da8c77ec81974b1bef201
anonym-angel <anonym-angel@yandex.ru>
2018-05-30 13:28:50 +0300

    add class Fight

--- main.py
+++ main.py
@@ -6,21 +6,23 @@ class Animal:
         self.list_animal = ['медведь', 'волк', 'тигр', 'баран', 'крокодил', 'змея', 'носорог', 'курица',
                             'енот', 'кошка']
         self.random_list_animal = random.choice(self.list_animal)
-        print(self.random_list_animal)
-
-    def dangerous(self, list_animal):
-        pass
 
 
 class Human(Animal):
     def __init__(self):
-        self.list_gun = ['ружье', 'ничего', 'палка', 'топор']
+        super().__init__()
+        self.list_gun = ['ружье', 'ничего', 'палку', 'топор']
         self.random_list_gun = random.choice(self.list_gun)
-        print(self.random_list_gun)
+
 
 class Fight(Human):
+    def __init__(self):
+        super().__init__()
+        print('На арене встречаются {} и человек'.format(self.random_list_animal))
+        print('Человек получает {} в качестве оружия'.format(self.random_list_gun))
 
 
 if __name__ == '__main__':
     Animal()
     Human()
+    Fight()
