e243f7d52d84e1a01ebe39a3a74822227d83827e
anonym-angel <anonym-angel@yandex.ru>
2018-05-31 00:16:17 +0300

    fixed class inheritance/change class Victory

--- main.py
+++ main.py
@@ -9,28 +9,35 @@ class Animal:
                             'енот', 'кошка']
         self.random_list_animal = random.choice(self.list_animal)
 
+    def random_animal(self):
+        return self.random_list_animal
 
-class Human(Animal):
+
+class Human:
     hu_counter = 0
 
     def __init__(self):
-        super().__init__()
-        self.list_gun = ['ружье', 'ничего', 'палку', 'ядовитый дротик']
+        self.list_gun = ['ружье', 'резиновую уточку', 'палку', 'ядовитый дротик']
         self.random_list_gun = random.choice(self.list_gun)
 
+    def random_gun(self):
+        return self.random_list_gun
 
-class Fight(Human):
-    def arena(self):
-        print('На арене встречаются {} и человек'.format(self.random_list_animal))
-        print('Человек получает {} в качестве оружия'.format(self.random_list_gun))
+
+class Fight:
+    def __init__(self):
+        self.animal = Animal()
+        self.gun = Human()
+        print('На арене встречаются {} и человек'.format(self.animal.random_animal()))
+        print('Человек получает {} в качестве оружия'.format(self.gun.random_gun()))
 
     def weapon_inspection(self):
-        gun = self.random_list_gun
-        animal = self.random_list_animal
-        if (animal == 'енот') or (animal == 'овца') or (animal == 'змея') & (gun != 'ничего'):
+        gun = self.gun.random_gun()
+        animal = self.animal.random_animal()
+        if (animal == 'енот') or (animal == 'овца') or (animal == 'змея') & (gun != 'резиновую уточку'):
             print('Человек выиграл, +1 очко\n')
             Human.hu_counter += 1
-        elif (animal == 'медведь') or (animal =='волк') or (animal == 'тигр') or (animal == 'крокодил') &\
+        elif (animal == 'медведь') or (animal == 'волк') or (animal == 'тигр') or (animal == 'крокодил') &\
                 (gun == 'ружье') or (gun == 'ядовитый дротик'):
             print('Человек выиграл, +1 очко\n')
             Human.hu_counter += 1
@@ -45,21 +52,22 @@ class Fight(Human):
             Animal.an_counter += 1
 
 
-class Victory:
-    def __init__(self, battle=0):
+class Tournament:
+    def __init__(self, battles=5):
+        self.battles = battles
 
-        while battle < 5:
+    def fight(self):
+        for round_battles in range(self.battles):
             one = Fight()
-            one.arena()
             one.weapon_inspection()
-            battle += 1
         print('Человек выиграл {} раз/а'.format(Human.hu_counter))
         print('Животное выиграло {} раз/а\n'.format(Animal.an_counter))
         if Human.hu_counter > Animal.an_counter:
-            print('В этой бою выиграл человек')
+            print('В этом бою выиграл человек')
         else:
             print('В этом бою выиграло животное')
 
 
 if __name__ == '__main__':
-    Victory()
+    two = Tournament()
+    two.fight()
