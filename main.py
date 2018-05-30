4ce0e6534d4795a57cbebb3af8dcef59c7a182a1
anonym-angel <anonym-angel@yandex.ru>
2018-05-30 12:48:55 +0300

    add class Anumal and class Human

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import random
 
 
+class Animal:
+    def __init__(self):
+        self.list_animal = ['медведь', 'волк', 'тигр', 'баран', 'крокодил', 'змея', 'носорог', 'курица',
+                            'енот', 'кошка']
+        self.random_list_animal = random.choice(self.list_animal)
+        print(self.random_list_animal)
 
+    def dangerous(self, list_animal):
+        pass
+
+
+class Human(Animal):
+    def __init__(self):
+        self.list_gun = ['ружье', 'ничего', 'палка', 'топор']
+        self.random_list_gun = random.choice(self.list_gun)
+        print(self.random_list_gun)
+
+class Fight(Human):
 
 
 if __name__ == '__main__':
-    pass
+    Animal()
+    Human()
